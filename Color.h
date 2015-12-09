5ddcd6bdafaac7d5e2d3c6e3966219dcc9187c6b
Anil Siruvolu <siruvolu@icloud.com>
2015-12-09 18:46:03 -0500

    Latest Files

---
+++ Color.h
@@ -0,0 +1,25 @@
+//
+//  Color.h
+//  GraphTest
+//
+//  Created by Anil Siruvolu on 12/8/15.
+//  Copyright © 2015 Anil Siruvolu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Color)
+
++ (instancetype)fsRed;
++ (instancetype)fsOrange;
++ (instancetype)fsYellow;
++ (instancetype)fsGreen;
++ (instancetype)fsLightBlue;
++ (instancetype)fsDarkBlue;
++ (instancetype)fsPurple;
++ (instancetype)fsPink;
++ (instancetype)fsDarkGray;
++ (instancetype)fsLightGray;
+
+
+@end
